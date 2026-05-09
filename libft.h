c6713f621277d2ec51f71af361906d509fa3bac1
Khaled Rahnama <krahnama@c3a8c1.42berlin.de>
2026-05-09 12:49:38 +0200

    general test

--- libft.h
+++ libft.h
@@ -6,19 +6,19 @@
 /*   By: krahnama <krahnama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 09:55:17 by khaledrahna       #+#    #+#             */
-/*   Updated: 2026/05/08 21:22:31 by krahnama         ###   ########.fr       */
+/*   Updated: 2026/05/09 12:15:52 by krahnama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <stddef.h> 
-size_t ft_strlen(const char *s);
-int ft_isdigit(int c);
-int ft_isalpha(int c);
-int ft_isprint(int c);
-int ft_isascii(int c);
+# include <stddef.h>
 
+size_t	ft_strlen(const char *s);
+int		ft_isdigit(int c);
+int		ft_isalpha(int c);
+int		ft_isprint(int c);
+int		ft_isascii(int c);
 
 #endif
