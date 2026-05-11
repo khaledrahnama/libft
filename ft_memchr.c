235720faf7ed2e367efb73752c2ff35cc335ba33
Khaled Rahnama <krahnama@c4a14c10.42berlin.de>
2026-05-11 20:32:16 +0200

    final memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,36 +6,37 @@
 /*   By: krahnama <krahnama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 21:11:11 by krahnama          #+#    #+#             */
-/*   Updated: 2026/05/11 20:25:52 by krahnama         ###   ########.fr       */
+/*   Updated: 2026/05/11 20:31:49 by krahnama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <stdio.h>
-#include"libft.h"
 
-void *ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-       const unsigned char* start;
-        start = s;
-    while(n--)
-    {
-        if( *start++ == (unsigned char) c)
-        return (void*)--start;  
-    }
-    return NULL;
-}
+	const unsigned char	*start;
 
-int main()
+	start = s;
+	while (n--)
+	{
+		if (*start == (unsigned char)c)
+			return ((void *)start);
+		start++;
+	}
+	return (NULL);
+}
+/*
+int	main(void)
 {
-    char str[]="abcdefghi";
+	char str[]="abcdefghi";
 
-    char *ptr;
-    ptr = ft_memchr(str, 'd' , 6);
+	char *ptr;
+	ptr = ft_memchr(str, 'd' , 6);
 
-    printf("pointer is %p \n",ptr);
-    printf("value is %c \n",*ptr);
-    printf("value is %s \n",ptr);
+	printf("pointer is %p \n",ptr);
+	printf("value is %c \n",*ptr);
+	printf("value is %s \n",ptr);
 
-
-    return 0;
-}
+	return (0);
+} */
