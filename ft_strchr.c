4ca8bb8accb150c7b0e38a111e5c0881c2b94354
Khaled Rahnama <krahnama@c4a14c10.42berlin.de>
2026-05-11 21:54:13 +0200

    added strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -3,11 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: krahnama <krahnama@student.42berlin.d      +#+  +:+       +#+        */
+/*   By: krahnama <krahnama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 21:10:41 by krahnama          #+#    #+#             */
-/*   Updated: 2026/05/10 21:10:43 by krahnama         ###   ########.fr       */
+/*   Updated: 2026/05/11 21:53:59 by krahnama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strchr(const char *s, int c)
+{
+	const char	*char_ptr;
+	int			i;
+
+	char_ptr = s;
+	i = 0;
+	while (char_ptr[i])
+	{
+		if (*char_ptr == (char)c)
+			return ((char *)char_ptr);
+		char_ptr++;
+		i++;
+	}
+	if (*char_ptr == (char)c)
+		return ((char *)char_ptr);
+	return (NULL);
+}
+
+/* #include<stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+	printf("%s \n", ft_strchr("something",'t'));
+} */
