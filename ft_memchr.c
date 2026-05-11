c6afa9e162ce0862744e861dc0528228036d563e
Khaled Rahnama <krahnama@c4a14c10.42berlin.de>
2026-05-11 21:29:19 +0200

    memcmp final

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,12 +6,11 @@
 /*   By: krahnama <krahnama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 21:11:11 by krahnama          #+#    #+#             */
-/*   Updated: 2026/05/11 20:31:49 by krahnama         ###   ########.fr       */
+/*   Updated: 2026/05/11 20:35:18 by krahnama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
@@ -27,6 +26,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	return (NULL);
 }
 /*
+#include <stdio.h>
 int	main(void)
 {
 	char str[]="abcdefghi";
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -3,11 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: krahnama <krahnama@student.42berlin.d      +#+  +:+       +#+        */
+/*   By: krahnama <krahnama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 21:11:30 by krahnama          #+#    #+#             */
-/*   Updated: 2026/05/10 21:11:32 by krahnama         ###   ########.fr       */
+/*   Updated: 2026/05/11 21:27:54 by krahnama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*s1_ptr;
+	const unsigned char	*s2_ptr;
+
+	s1_ptr = s1;
+	s2_ptr = s2;
+	while (n--)
+	{
+		if (*s1_ptr != *s2_ptr)
+			return (*s1_ptr - *s2_ptr);
+		s1_ptr++;
+		s2_ptr++;
+	}
+	return (0);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char s1[] = "abcdffghsdfsdfsdfsdfsdfsdfsdfsdfsdfsdf";
+	char s2[] = "abcderwerwerwerewrwererwrwerwerwerwvvsdf";
+
+	printf("%d \n", ft_memcmp(s1, s2, 4));
+	printf("%d \n", ft_memcmp(s1, s2, 6));
+} */
