a1c3fdb7d7eb4f4b39fc49ed11782768f44fbbbf
Khaled Rahnama <krahnama@c3a5c1.42berlin.de>
2026-05-17 16:19:41 +0200

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,11 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: krahnama <krahnama@student.42berlin.d      +#+  +:+       +#+        */
+/*   By: krahnama <krahnama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 21:12:25 by krahnama          #+#    #+#             */
-/*   Updated: 2026/05/10 21:12:27 by krahnama         ###   ########.fr       */
+/*   Updated: 2026/05/17 15:55:28 by krahnama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include<stdio.h>
+#include <bsd/bsd.h>
 
+int main()
+{
+    printf("location of substring : %s" , strnstr("some strings that we search that for a substring" , "that" , 50) );
+}
+
+char * strnstr(const char *big, const char *little, size_t len)
+{
+    size_t i;
+    i = 0;
+    size_t j;
+   
+    if(*little == '\0')
+    return((char *) big);
+
+    while(i < len)
+    {
+        j=0;
+        while((i+j)<len 
+        && little[j] 
+        && big[i+j]
+        && (big[i+j]==little[j]))
+        {j++;}
+        if(little[j]=='\0')
+        return((char*)&big[i]);
+        i++;
+    }
+    return(NULL);
+}
