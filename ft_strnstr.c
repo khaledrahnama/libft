b385b65445a3c46cf5e2858c68be20dae1dbb5a9
Khaled Rahnama <krahnama@c4a11c4.42berlin.de>
2026-05-19 19:25:43 +0200

    ft_strstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,19 +6,19 @@
 /*   By: krahnama <krahnama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 21:12:25 by krahnama          #+#    #+#             */
-/*   Updated: 2026/05/17 15:55:28 by krahnama         ###   ########.fr       */
+/*   Updated: 2026/05/19 17:59:19 by krahnama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stdio.h>
 #include <bsd/bsd.h>
 
-int main()
+/* int main()
 {
     printf("location of substring : %s" , strnstr("some strings that we search that for a substring" , "that" , 50) );
-}
+} */
 
-char * strnstr(const char *big, const char *little, size_t len)
+char * ft_strnstr(const char *big, const char *little, size_t len)
 {
     size_t i;
     i = 0;
@@ -40,4 +40,16 @@ char * strnstr(const char *big, const char *little, size_t len)
         i++;
     }
     return(NULL);
+}
+
+#include<stdio.h>
+int main()
+{
+    char *big = {"something that we need to search inside"};
+    char *little={"to"};
+
+    char *result;
+    result = ft_strnstr(big, little, 30);
+    printf(" the result is : %s", result);
+    
 }
