23feb1d492a76e0431115b9b0c72354334b53116
Khaled Rahnama <krahnama@c4a13c5.42berlin.de>
2026-05-19 21:27:32 +0200

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: krahnama <krahnama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 21:10:21 by krahnama          #+#    #+#             */
-/*   Updated: 2026/05/19 21:20:55 by krahnama         ###   ########.fr       */
+/*   Updated: 2026/05/19 21:26:16 by krahnama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,14 @@ size_t ft_strlcat(char *dst, const char *src, size_t size)
   
 
     size_t dst_len;
-    dst_len = ft_strlen(dst_len);
+    dst_len = ft_strlen(dst);
     size_t src_len;
-    src_len = ft_strlen(src_len);
+    src_len = ft_strlen(src);
 
+     if (size <= dst_len)
+        return (size + src_len);
 
-    while(src[i] && (dst_len + i)< size)
+    while(src[i] && (dst_len + i)< size - 1)
     {
         dst[dst_len + i]= src[i];
         i++;
@@ -40,6 +42,9 @@ size_t ft_strlcat(char *dst, const char *src, size_t size)
 
 int main()
 {
-    printf("%s", ft_strlcat("something", " more", 30));
+    char dst[30] = "something";
+    
+    printf("%zu", ft_strlcat(dst, " more", 30));
+
 }
 
