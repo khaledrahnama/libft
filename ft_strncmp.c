854e620c0afdaaf261675e37b0fd3cd08f80663d
Khaled Rahnama <krahnama@c3a5c2.42berlin.de>
2026-05-31 00:33:29 +0200

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,11 +6,28 @@
 /*   By: krahnama <krahnama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 21:11:44 by krahnama          #+#    #+#             */
-/*   Updated: 2026/05/30 19:16:14 by krahnama         ###   ########.fr       */
+/*   Updated: 2026/05/31 00:33:18 by krahnama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (i < n && s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	if (i == n)
+		return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+
+/*
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
@@ -27,7 +44,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
-/* #include<string.h>
+
+
+#include<string.h>
 #include <stdio.h>
 
 int	main(void)
