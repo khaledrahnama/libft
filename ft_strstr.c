712e7676c84ce770a9cbf5b09a5e0ccb57424cac
Khaled Rahnama <krahnama@c3a5c2.42berlin.de>
2026-05-31 03:59:01 +0200

    ft_strstr.c

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,38 +6,38 @@
 /*   By: krahnama <krahnama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 18:01:51 by krahnama          #+#    #+#             */
-/*   Updated: 2026/05/30 11:03:09 by krahnama         ###   ########.fr       */
+/*   Updated: 2026/05/31 03:58:45 by krahnama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strstr(const char *haystack, const char *needle)
+char	*ft_strstr(const char *haystack, const char *needle)
 {
-    size_t i;
-    size_t j;
-    if(*needle == '\0')
-    return((char*)haystack);
+	size_t	i;
+	size_t	j;
 
-    i=0;
-    while(haystack[i])
-    {
-        j=0;
-        while(haystack[i+j]==needle[j])
-        {
-            j++;
-            
-        }
-        if(needle[j] == '\0')
-            return ((char*)&haystack[i]);
-        i++;
-    }
-    return(NULL);
+	if (*needle == '\0')
+		return ((char *)haystack);
+	i = 0;
+	while (haystack[i])
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j])
+		{
+			j++;
+		}
+		if (needle[j] == '\0')
+			return ((char *)&haystack[i]);
+		i++;
+	}
+	return (NULL);
 }
-
 
 /* #include <stdio.h>
  int main()
 {
-    printf("location of substring : %s" , ft_strstr("some strings thatthat we search that for a substring" , "that") );
+	printf("location of substring : %s" ,
+		ft_strstr("some strings thatthat we search that for a substring" ,
+			"that") );
 }  */
